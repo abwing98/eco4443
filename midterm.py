f751b61b0314eafa7f905e28fc044dd8ba2252ac
abwing98 <57374718+abwing98@users.noreply.github.com>
2019-11-05 11:08:42 -0500

    Update midterm.py

--- midterm.py
+++ midterm.py
@@ -5,10 +5,8 @@ Created on Mon Nov  4 11:03:14 2019
 
 @author: Adam
 """
+
 from pandas import read_csv
-
-data = read_csv('/Users/Adam/sales.csv',delimiter=',',)
-
 import numpy as np
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -16,86 +14,96 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from statsmodels.api import add_constant
 from pandas import DataFrame
-
-price = DataFrame(data.price)
-year = DataFrame(data.year)
-home_size = DataFrame(data.home_size)
-parcel_size = DataFrame(data.parcel_size)
-beds = DataFrame(data.beds)
-age = DataFrame(data.age)
-pool = DataFrame(data.pool)
-cbd_dist = DataFrame(data.cbd_dist)
-x_coord = DataFrame(data.x_coord)
-y_coord = DataFrame(data.y_coord)
-
 import pandas as pd
+from sklearn import model_selection
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn import metrics
+
+fulldata = read_csv('/Users/Adam/sales.csv',delimiter=',',)
+
+#MODEL 1
+data = read_csv('/Users/Adam/sales.csv', delimiter=',' , usecols=(1,2,3,4,5,6,7,8,9))
+#pandas dataframe
 df = pd.DataFrame(data)
-corrmatrix = df.corr()
+#define target (dependent) variable as y
+y = pd.DataFrame(fulldata.price)
+#create training/testing vars
+x_train, x_test, y_train, y_test = train_test_split(df, y, test_size=0.1)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+#fit a model
+model = LinearRegression()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+#plot the model
+plt.scatter(y_test, predictions)
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+#score the model
+print("Score:", model.score(x_test,y_test))
+#Perform 10-Fold Cross Validation
+scores = cross_val_score(model, df, y, cv=10)
+print("Cross-validated scores:", scores)
+#Make cross validated predictions
+predictions = cross_val_predict(model, df, y, cv=10)
+plt.scatter(y, predictions)
+#Check Rsquared
+accuracy = metrics.r2_score(y, predictions)
+print("Cross-Predicted Accuracy:", accuracy)
 
-#scale price data
-data.price = data.price/1000
+results = model.score(x_test, y_test)
+print("Accuracy: %.3f%%" % (results*100.0))
 
-#split dependent data into training/testing sets
-y_train = data.price[:8000]
-y_test = data.price[8000:]
 
-model = smf.ols('price ~ home_size + x_coord + y_coord + age', data=data[:8000])
-results = model.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
+#MODEL 2
+fulldata = read_csv('/Users/Adam/sales.csv',delimiter=',',)
+data = read_csv('/Users/Adam/sales.csv', delimiter=',' , usecols=(1,2,3,4,5,6,7,8,9))
+#define target (dependent) variable as y
+y = pd.DataFrame(fulldata.price)
+#create exponential variables
+hsize2 = np.power(data.home_size,2)
+hsize3 = np.power(data.home_size,3)
+hsize4 = np.power(data.home_size,4)
+beds2 = np.power(data.beds,2)
+beds3 = np.power(data.beds,3)
+beds4 = np.power(data.beds,4)
+cbd_hsize = np.multiply(data.cbd_dist, data.home_size)
+cbd_beds = np.multiply(data.cbd_dist, data.beds) 
+beds_hsize = np.multiply(data.beds, data.home_size)
+#create new dataframe with our models variables: home size^1,2,3,4; beds^1,2,3,4; cbd_dist*home_size; cbd_dist*beds; beds*home_size
+new = [data.home_size, hsize2, hsize3, hsize4, data.beds, beds2, beds3, beds4, data.cbd_dist, cbd_hsize, cbd_beds, beds_hsize]
+df = pd.DataFrame(new)
+df = np.transpose(df)
 
-train_mse = sum((y_train - pred_train)**2)/results.nobs
-test_mse = sum((y_test - pred_test)**2)/len(y_test)
+#create training/testing vars
+x_train, x_test, y_train, y_test = train_test_split(df, y, test_size=0.1)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+#fit a model
+model = LinearRegression()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+#plot the model
+plt.scatter(y_test, predictions)
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+#score the model
+print("Score:", model.score(x_test,y_test))
+#Perform 10-Fold Cross Validation
+scores = cross_val_score(model, df, y, cv=10)
+print("Cross-validated scores:", scores)
+#Make cross validated predictions
+predictions = cross_val_predict(model, df, y, cv=10)
+plt.scatter(y, predictions)
+#Check Rsquared
+accuracy = metrics.r2_score(y, predictions)
+print("Cross-Predicted Accuracy:", accuracy)
 
-model1 = smf.ols('price ~ home_size + np.power(home_size, 2) + np.power(home_size, 3) + np.power(home_size, 4) + cbd_dist + np.power(cbd_dist, 2) + np.power(cbd_dist, 3) + beds + np.power(beds, 2) + np.power(beds, 3) + np.power(beds, 4) + parcel_size + np.power(parcel_size, 2) + np.power(parcel_size, 3) + np.power(parcel_size, 4)', data=(data[:8000]))
-results = model1.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
-
-train_mse1 = sum((y_train - pred_train)**2)/results.nobs
-test_mse1 = sum((y_test - pred_test)**2)/len(y_test)
-
-model2 = smf.ols('price ~ beds + home_size + cbd_dist + year + parcel_size + x_coord + y_coord + age + np.power(age, 2)', data=(data[:8000]))
-results = model2.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
-
-train_mse2 = sum((y_train - pred_train)**2)/results.nobs
-test_mse2 = sum((y_test - pred_test)**2)/len(y_test)
-
-model3 = smf.ols('price ~ home_size + np.multiply(home_size, beds) + cbd_dist + beds + np.multiply(beds, parcel_size) + parcel_size + np.multiply(parcel_size, home_size)', data=(data[:8000]))
-results = model3.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
-
-train_mse3 = sum((y_train - pred_train)**2)/results.nobs
-test_mse3 = sum((y_test - pred_test)**2)/len(y_test)
-
-model4 = smf.ols('price ~ home_size + np.power(home_size, 2) + np.power(home_size, 3) + np.power(home_size, 4) + cbd_dist + beds + np.power(beds, 2) + np.power(beds, 3) + np.power(beds, 4) + parcel_size + np.power(parcel_size, 2) + np.power(parcel_size, 3) + np.power(parcel_size, 4)', data=(data[:8000]))
-results = model4.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
-
-train_mse4 = sum((y_train - pred_train)**2)/results.nobs
-test_mse4 = sum((y_test - pred_test)**2)/len(y_test)
-
-model5 = smf.ols('price ~ home_size + np.power(home_size, 2) + np.power(home_size, 3) + np.power(home_size, 4) + beds + np.power(beds, 2) + np.power(beds, 3) + np.power(beds, 4) + cbd_dist + np.multiply(cbd_dist, home_size)', data=(data[:8000]))
-results = model5.fit()
-print(results.summary())
-print(results.summary())
-pred_train = results.predict(data[:8000])
-pred_test  = results.predict(data[8000:])
-
-train_mse5 = sum((y_train - pred_train)**2)/results.nobs
-test_mse5 = sum((y_test - pred_test)**2)/len(y_test)
-
+results = model.score(x_test, y_test)
+print("Accuracy: %.3f%%" % (results*100.0))
